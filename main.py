2884f7047ff1fc5bae00283a7bfd82ea85d67901
Bethel Yohannes <bethelyohannes4@gmail.com>
2023-05-23 16:21:35 +0300

    add new function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ast import List
 from csv import DictWriter, writer
+import csv
 import json
 import time
 from fastapi import FastAPI,Request
@@ -13,11 +14,106 @@ from keras.models import load_model
 from pydantic import BaseModel
 from typing import List
 from model import movieModel
- 
+from fastapi.middleware.cors import CORSMiddleware
+from keras.models import load_model
+from keras.models import load_model
+
 app = FastAPI()
-@app.get('/')
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+def movieRcommendation(model):
+     movie_df = pd.read_csv("movies.csv")
+     df = pd.read_csv("ratings.csv")
+     users = df.userId.unique()
+     movies = df.movieId.unique()
+     userid2idx = {o:i for i,o in enumerate(users)}
+     movieid2idx = {o:i for i,o in enumerate(movies)}
+     user_id = 4562
+     movies_watched_by_user = df[df.userId == user_id]
+     movies_not_watched = movie_df[
+    ~movie_df["movieId"].isin(movies_watched_by_user.movieId.values)
+ ]["movieId"]
+     movies_not_watched = list(
+    set(movies_not_watched).intersection(set(movieid2idx.keys()))
+ )
+     movies_not_watched = [[movieid2idx.get(x)] for x in movies_not_watched]
+     user_encoder = userid2idx.get(user_id)  
+     user_id = [[user_encoder]] * len(movies_not_watched)
+     movies_not_watched = np.array(movies_not_watched)
+     user_id = np.array(user_id)
+     ratings = model.predict([np.array(user_id),np.array(movies_not_watched)]).flatten()
+     top_ratings_indices = ratings.argsort()[-10:][::-1]
+     recommended_movie_ids = [
+     movieid2idx.get(movies_not_watched[x][0]) for x in top_ratings_indices
+  ]
+
+     print("Showing recommendations for user: {}".format(user_id))
+     print("====" * 9)
+     print("MoviSes with high ratings from user")
+     print("----" * 8)
+     top_movies_user = (
+     movies_watched_by_user.sort_values(by="rating", ascending=False)
+     .head(5)
+     .movieId.values
+)
+     movie_df_rows = movie_df[movie_df["movieId"].isin(top_movies_user)]
+     for row in movie_df_rows.itertuples():
+        print(row.title, ":", row.genres)
+
+     print("----" * 8)
+     print("Top 10 movie recommendations")
+     print("----" * 8)
+     recommended_movies = movie_df[movie_df["movieId"].isin(recommended_movie_ids)]
+     movies = []
+     for row in recommended_movies.itertuples():
+        movies.append({ 
+            "title":row.title,
+                   "genere" :row.genres,
+                   "movieId":row.movieId
+                    })
+  
+        return movies
+
+@app.get('/movies')
 async def root():
-    return {"example": "this is a test","data":0}
+    jsonArray = []
+    moviecatagories = []
+    #read csv file
+    with open('movies.csv', encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+  
+    #convert python jsonArray to JSON String and write to file
+ 
+# Filter python objects with list comprehensions
+    for i in jsonArray:
+        dict = {'id':i['genres'],'genres':i['genres'],'movies':[{'title':i['title'],'movieId':i['movieId']}]}
+        if(i['genres']=='Comedy'):
+            moviecatagories.append(dict)
+        if(i['genres']=='Action'):
+             moviecatagories.append(dict)
+        if(i['genres']=='Drama'):
+             moviecatagories.append(dict)
+        if(i['genres']=='Romance'):
+             moviecatagories.append(dict)
+# Transform python object back into json
+   
+# Show json
+    # print (moviecatagories)
+
+    return moviecatagories
 
 
 class Movie(BaseModel):
@@ -30,8 +126,8 @@ class Movie(BaseModel):
 async def create_recommendation(movies:list[Movie]):
       field_names = ['userId','movieId','rating','timestamp']
       ts = time.time()
-      List = [{"userId":4562, "movieId":5532,"rating":5 ,"timestamp":int(ts)},
-              {"userId":4562, "movieId":7845, "rating":5  ,"timestamp":int(ts)},
+      List = [{"userId":4562, "movieId":639,"rating":5 ,"timestamp":int(ts)},
+              {"userId":4562, "movieId":742, "rating":5  ,"timestamp":int(ts)},
               {"userId":4562,  "movieId":1785, "rating":5  ,"timestamp":int(ts)}]
    
       for i in List:
@@ -40,14 +136,9 @@ async def create_recommendation(movies:list[Movie]):
             dictwriter_object = DictWriter(f_object, fieldnames=field_names)
             dictwriter_object.writerow(i)
             f_object.close()
-      movie_field = ['movieId','title','genres']
-      for i in movies:
-        with open('movies.csv','a') as f_objects:
-              movie_dict = DictWriter(f_objects,fieldnames=movie_field)
-              movie_dict.writerow({"movieId":i.movieId,"title":i.title,"genres":i.genres})
-              f_objects.close()
-      response =   movieModel()
-      return response
+    #   movieModel()
+      new_model = load_model('movie_recommendation.h5')
+      return movieRcommendation(new_model)
 
 
 #  add to datasets, generate a new user id, movies
--- model.py
+++ model.py
@@ -15,7 +15,7 @@ import random as rn
 
 def movieModel():
     df = pd.read_csv("ratings.csv")
-    movie_df = pd.read_csv("movies.csv")
+    # movie_df = pd.read_csv("movies.csv")
     df['rating'] = df['rating'].fillna(0)
     users = df.userId.unique()
     movies = df.movieId.unique()
@@ -41,51 +41,6 @@ def movieModel():
     History = model.fit([train.userId,train.movieId],train.rating, batch_size=64,
                               epochs =10, validation_data = ([valid.userId,valid.movieId],valid.rating),
                               verbose = 1)
-    user_id = df.userId.sample(1).iloc[0]
-  # user_id = 4452
-    movies_watched_by_user = df[df.userId == user_id]
-    movies_not_watched = movie_df[
-    ~movie_df["movieId"].isin(movies_watched_by_user.movieId.values)
- ]["movieId"]
-    movies_not_watched = list(
-    set(movies_not_watched).intersection(set(movieid2idx.keys()))
- )
-    movies_not_watched = [[movieid2idx.get(x)] for x in movies_not_watched]
-    user_encoder = userid2idx.get(user_id)  
-    user_id = [[user_encoder]] * len(movies_not_watched)
-    movies_not_watched = np.array(movies_not_watched)
-    user_id = np.array(user_id)
-    ratings = model.predict([np.array(user_id),np.array(movies_not_watched)]).flatten()
-    top_ratings_indices = ratings.argsort()[-10:][::-1]
-    recommended_movie_ids = [
-    movieid2idx.get(movies_not_watched[x][0]) for x in top_ratings_indices
-  ]
-
-    print("Showing recommendations for user: {}".format(user_id))
-    print("====" * 9)
-    print("Movies with high ratings from user")
-    print("----" * 8)
-    top_movies_user = (
-    movies_watched_by_user.sort_values(by="rating", ascending=False)
-    .head(5)
-    .movieId.values
-)
-    movie_df_rows = movie_df[movie_df["movieId"].isin(top_movies_user)]
-    for row in movie_df_rows.itertuples():
-        print(row.title, ":", row.genres)
-
-    print("----" * 8)
-    print("Top 10 movie recommendations")
-    print("----" * 8)
-    recommended_movies = movie_df[movie_df["movieId"].isin(recommended_movie_ids)]
-  # print([recommended_movies])
-  # return recommended_movies
-    movies = []
-    for row in recommended_movies.itertuples():
-        movies.append({ 
-            "title":row.title,
-                   "genere" :row.genres,
-                   "movieId":row.movieId
-                    })
-  
-    return movies
+    model.save('movie_recommendation.h5')
+    
+    
